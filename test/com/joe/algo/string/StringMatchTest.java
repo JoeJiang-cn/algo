9964f1e7ad09609e62fa8e6fd4f84e4dac675b9e
Joe.Jiang <joe.jiang@fanruan.com>
2021-08-29 21:28:41 +0800

    KMP Test

--- test/com/joe/algo/string/StringMatchTest.java
+++ test/com/joe/algo/string/StringMatchTest.java
@@ -39,4 +39,15 @@ public class StringMatchTest {
         Assert.assertEquals(StringMatch.BM(main, 6, pattern2, 2), 1);
         Assert.assertEquals(StringMatch.BM(main, 6, pattern3, 2), -1);
     }
+
+    @Test
+    public void testKMP() {
+        char[] main = {'a', 'b', 'a', 'b', 'b', 'a'};
+        char[] pattern1 = {'a', 'b', 'b'};
+        char[] pattern2 = {'b', 'a'};
+        char[] pattern3 = {'c', 'b'};
+        Assert.assertEquals(StringMatch.KMP(main, 6, pattern1, 3), 2);
+        Assert.assertEquals(StringMatch.KMP(main, 6, pattern2, 2), 1);
+        Assert.assertEquals(StringMatch.KMP(main, 6, pattern3, 2), -1);
+    }
 }
