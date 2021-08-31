01f32b022fb24279492651dd684ebaa0a50d8ed0
Joe.Jiang <joe.jiang@fanruan.com>
2021-08-31 23:28:32 +0800

    add test class

---
+++ test/com/joe/algo/structure/LRUCacheTest.java
@@ -0,0 +1,38 @@
+package com.joe.algo.structure;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class LRUCacheTest {
+
+    @Test
+    public void testLRUCache() {
+        LRUCache lruCache = new LRUCache(10);
+        lruCache.put(10, 13);
+        lruCache.put(3, 17);
+        lruCache.put(6, 11);
+        lruCache.put(10, 5);
+        lruCache.put(9, 10);
+        Assert.assertEquals(lruCache.get(13), -1);
+        lruCache.put(2, 19);
+        Assert.assertEquals(lruCache.get(2), 19);
+        Assert.assertEquals(lruCache.get(3), 17);
+        lruCache.put(5, 25);
+        Assert.assertEquals(lruCache.get(8), -1);
+        lruCache.put(9, 22);
+        lruCache.put(5, 5);
+        lruCache.put(1, 30);
+        Assert.assertEquals(lruCache.get(11), -1);
+        lruCache.put(9, 12);
+        Assert.assertEquals(lruCache.get(7), -1);
+        Assert.assertEquals(lruCache.get(5), 5);
+        Assert.assertEquals(lruCache.get(8), -1);
+        Assert.assertEquals(lruCache.get(9), 12);
+        lruCache.put(4, 30);
+        lruCache.put(9, 3);
+        Assert.assertEquals(lruCache.get(9), 3);
+        Assert.assertEquals(lruCache.get(10), 5);
+        Assert.assertEquals(lruCache.get(10), 5);
+    }
+
+}
---
+++ test/com/joe/algo/structure/TrieTest.java
@@ -0,0 +1,33 @@
+package com.joe.algo.structure;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TrieTest {
+    Trie trie;
+
+    @Before
+    public void setUp() {
+        trie = new Trie();
+        trie.deriveInsert("hello")
+                .deriveInsert("her")
+                .deriveInsert("happy")
+                .deriveInsert("world")
+                .deriveInsert("word");
+    }
+
+    @Test
+    public void testPerfectMatch() {
+        Assert.assertTrue(trie.perfectMatch("her"));
+        Assert.assertTrue(trie.perfectMatch("word"));
+        Assert.assertFalse(trie.perfectMatch("wo"));
+    }
+
+    @Test
+    public void testIsPrefix() {
+        Assert.assertTrue(trie.isPrefix("her"));
+        Assert.assertTrue(trie.isPrefix("he"));
+        Assert.assertFalse(trie.isPrefix("wocao"));
+    }
+}
